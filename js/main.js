1bd6326641ca8b36bce16915132be675e51f08f4
lynavleder <31221787+lynavleder@users.noreply.github.com>
2020-02-12 13:45:49 -0700

    Site update

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  } 
+
+  // Image Slider
+$(document).ready(function(){
+    $('.img-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+
+// $(document).ready(function(){
+//   $('.slider-for').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-nav'
+//   });
+//   $('.slider-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-for',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+//   });
+// });
